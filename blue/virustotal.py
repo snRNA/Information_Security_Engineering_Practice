40648116a3f1fdded8fa371fe844a0f00a1cda10
songnan666 <sn201411123015@gmail.com>
2017-07-14 18:47:32 +0800

    优化virustotal脚本，先计算md5获取文件报告

--- blue/virustotal.py
+++ blue/virustotal.py
@@ -4,6 +4,45 @@
 import requests
 import sys
 import os.path
+import hashlib
+import time
+
+
+def getMD5(filename):
+    f = open(filename, 'rb')
+    m = hashlib.md5()
+    while True:
+        data = f.read(4096)
+        if not data:
+            break
+        m.update(data)
+    return m.hexdigest()
+
+def getScanReport(resource):
+    params = {'apikey': '4f00ad19447f5c9269e00dcccf173ac458860bb0a8763e8e87af66dddcff962b','resource':resource}
+    headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "gzip,  My Python requests library example client or username"
+    }
+    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
+                           params=params, headers=headers)
+    report_response = response.json()
+
+    if (report_response['response_code'] == 1):
+        for i in report_response['scans']:
+            if (report_response['scans'][i]['detected'] == True):
+                print(i + " : " + report_response['scans'][i]['result'])
+                return True
+                #break
+    else:
+        return False
+
+def uploadfile(filename):
+    params = {'apikey': '4f00ad19447f5c9269e00dcccf173ac458860bb0a8763e8e87af66dddcff962b'}
+    files = {'file': (os.path.basename(filename), open(filename, 'rb'))}
+    response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
+    json_response = response.json()
+    return json_response
 
 
 if(len(sys.argv)<=1):
@@ -14,35 +53,18 @@ if(os.path.exists(sys.argv[1])!=True):
     print("file not found")
     sys.exit(1)
 
+md5 = getMD5(sys.argv[1])
 
-# 上传文件到virustotal，获取上传的文件的sha256值
+if(getScanReport(md5)):
+    exit(0)
+else:
+    json_response = uploadfile(sys.argv[1])
+    time.sleep(15)
+    if (getScanReport(json_response['resource']) == True):
+        exit(0)
+    else:
+        print('Safe file!')
 
-params = {'apikey': ''}
-files = {'file': (os.path.basename(sys.argv[1]), open(sys.argv[1], 'rb'))}
-response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
-json_response = response.json()
 
-#print(json_response)
 
-# 根据获取的文件的sha256，从api请求文件的扫描结果
 
-params = {'apikey': '', 'resource': json_response['resource']}
-#params = {'apikey': '', 'resource': '7e25f7f43df851ae777658910b865a1d077e4883a6a8979cbc599729e4e94020'}
-headers = {
-  "Accept-Encoding": "gzip, deflate",
-  "User-Agent" : "gzip,  My Python requests library example client or username"
-  }
-response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
-  params=params, headers=headers)
-report_response = response.json()
-
-#print(report_response)
-
-#print(report_response['scans'])
-
-# 若扫描结构中有检测到威胁警报，则输出
-
-for i in report_response['scans']:
-    if(report_response['scans'][i]['detected'] == True):
-        print(i +" : " + report_response['scans'][i]['result'])
-        break
